0d643d7dd8f1186ef3f90573f09cd2ebff96b0c0
whid-bb <whid11s50@gmail.com>
2022-05-24 14:57:39 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
+/* eslint-disable max-classes-per-file */
 class Ls {
-    constructor(booksKey) {
-        this.booksKey = booksKey
-    }
+  constructor(booksKey) {
+    this.booksKey = booksKey;
+  }
 
     addToLS = (bookObj) => {
-        localStorage.setItem(this.booksKey, JSON.stringify(bookObj));
+      localStorage.setItem(this.booksKey, JSON.stringify(bookObj));
     };
 
     getFromLS = (key) => {
-        const lsData = localStorage.getItem(key);
-        if (lsData) {
-            const data = JSON.parse(lsData);
-            return data;
-        }
+      const lsData = localStorage.getItem(key);
+      if (lsData) {
+        const data = JSON.parse(lsData);
+        return data;
+      }
 
-        return [];
+      return [];
     };
 
     removeFromLS = (bookId) => {
-        const bookdata = this.getFromLS(this.booksKey);
-        bookdata.filter((el, i) => {
-            if (el.id === Number(bookId)) {
-                bookdata.splice(i, 1);
-                this.addToLS(bookdata);
-            }
-            return true;
-        });
+      const bookdata = this.getFromLS(this.booksKey);
+      bookdata.filter((el, i) => {
+        if (el.id === Number(bookId)) {
+          bookdata.splice(i, 1);
+          this.addToLS(bookdata);
+        }
+        return true;
+      });
     };
 }
-
-// const form = document.getElementById('add-book');
-// const inputAuthor = document.getElementById('author');
-// const inputTitle = document.getElementById('title');
-// const bookList = document.getElementById('book-list');
 
 class BooksList extends Ls {
     books = []
 
     constructor(form, inputAuthor, inputTitle, bookListContainer, booksKey = 'books') {
-        super(booksKey)
-        this.form = form
-        this.inputAuthor = inputAuthor
-        this.inputTitle = inputTitle
-        this.bookListContainer = bookListContainer
-        this.booksKey = booksKey
+      super(booksKey);
+      this.form = form;
+      this.inputAuthor = inputAuthor;
+      this.inputTitle = inputTitle;
+      this.bookListContainer = bookListContainer;
+      this.booksKey = booksKey;
     }
 
     removeBook = (id, btnHTML) => {
-        btnHTML.target.parentNode.remove();
-        this.removeFromLS(id);
+      btnHTML.target.parentNode.remove();
+      this.removeFromLS(id);
     };
 
     renderHTML = (obj) => `
@@ -61,81 +57,72 @@ class BooksList extends Ls {
             `;
 
     findRemoveButtons = () => {
-        const removeBookBtn = document.querySelectorAll('.remove-book');
+      const removeBookBtn = document.querySelectorAll('.remove-book');
 
-        removeBookBtn.forEach((btn) => {
-            btn.addEventListener('click', (event) => {
-                const btnId = event.target.getAttribute('data-id');
+      removeBookBtn.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+          const btnId = event.target.getAttribute('data-id');
 
-                this.removeBook(btnId, event);
-            });
+          this.removeBook(btnId, event);
         });
+      });
     };
 
     addBookToHTML = (obj) => {
-        bookList.insertAdjacentHTML('afterbegin', this.renderHTML(obj));
-        this.findRemoveButtons();
+      this.bookListContainer.insertAdjacentHTML('afterbegin', this.renderHTML(obj));
+      this.findRemoveButtons();
     };
 
     addToBookList = (idBook, authorInput, titleInput) => {
-        const bookObj = {
-            id: idBook,
-            title: titleInput,
-            author: authorInput,
-        };
+      const bookObj = {
+        id: idBook,
+        title: titleInput,
+        author: authorInput,
+      };
 
-        this.books[this.booksKey] = this.getFromLS(this.booksKey);
-        this.books[this.booksKey].push(bookObj);
-        this.addBookToHTML(bookObj);
-        this.addToLS(this.books[this.booksKey]);
+      this.books[this.booksKey] = this.getFromLS(this.booksKey);
+      this.books[this.booksKey].push(bookObj);
+      this.addBookToHTML(bookObj);
+      this.addToLS(this.books[this.booksKey]);
     };
 
     getUniqueId = () => {
-        const data = this.getFromLS(this.booksKey);
-        let max = 0;
+      const data = this.getFromLS(this.booksKey);
+      let max = 0;
 
-        data.forEach((_, i) => {
-            if (data[i].id > max) {
-                max = data[i].id;
-            }
-        });
-        max += 1;
-        return max;
+      data.forEach((_, i) => {
+        if (data[i].id > max) {
+          max = data[i].id;
+        }
+      });
+      max += 1;
+      return max;
     };
 
     init() {
-        const data = this.getFromLS(this.booksKey);
-        if (data.length > 0) {
-            data.forEach((book) => {
-                this.addBookToHTML(book);
-            });
-        }
-
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-
-            const bookId = this.getUniqueId();
-
-            const authorText = this.inputAuthor.value;
-            const titleText = this.inputTitle.value;
-
-            this.addToBookList(bookId, authorText, titleText);
+      const data = this.getFromLS(this.booksKey);
+      if (data.length > 0) {
+        data.forEach((book) => {
+          this.addBookToHTML(book);
         });
-    };
+      }
+
+      this.form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const bookId = this.getUniqueId();
+
+        const authorText = this.inputAuthor.value;
+        const titleText = this.inputTitle.value;
+
+        this.addToBookList(bookId, authorText, titleText);
+      });
+    }
 }
-
-
-// new ls('books')
-
-// const booksKey = 'books';
-
-// const books = { books: [] };
 
 const form = document.getElementById('add-book');
 const inputAuthor = document.getElementById('author');
 const inputTitle = document.getElementById('title');
 const bookList = document.getElementById('book-list');
 
-new BooksList(form, inputAuthor, inputTitle, bookList).init()
-
-
+new BooksList(form, inputAuthor, inputTitle, bookList).init();
