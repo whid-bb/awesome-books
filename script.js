d918eedd3f77ef5cda1355dac9aad4a1348801d1
Christabelle143 <christabelleyouta143@gmail.com>
2022-05-24 15:19:30 +0100

    Add style.css codes

--- script.js
+++ script.js
@@ -4,120 +4,129 @@ class Ls {
     this.booksKey = booksKey;
   }
 
-    addToLS = (bookObj) => {
-      localStorage.setItem(this.booksKey, JSON.stringify(bookObj));
-    };
+  addToLS = (bookObj) => {
+    localStorage.setItem(this.booksKey, JSON.stringify(bookObj));
+  };
 
-    getFromLS = (key) => {
-      const lsData = localStorage.getItem(key);
-      if (lsData) {
-        const data = JSON.parse(lsData);
-        return data;
+  getFromLS = (key) => {
+    const lsData = localStorage.getItem(key);
+    if (lsData) {
+      const data = JSON.parse(lsData);
+      return data;
+    }
+
+    return [];
+  };
+
+  removeFromLS = (bookId) => {
+    const bookdata = this.getFromLS(this.booksKey);
+    bookdata.filter((el, i) => {
+      if (el.id === Number(bookId)) {
+        bookdata.splice(i, 1);
+        this.addToLS(bookdata);
       }
-
-      return [];
-    };
-
-    removeFromLS = (bookId) => {
-      const bookdata = this.getFromLS(this.booksKey);
-      bookdata.filter((el, i) => {
-        if (el.id === Number(bookId)) {
-          bookdata.splice(i, 1);
-          this.addToLS(bookdata);
-        }
-        return true;
-      });
-    };
+      return true;
+    });
+  };
 }
 
 class BooksList extends Ls {
-    books = []
+  books = [];
 
-    constructor(form, inputAuthor, inputTitle, bookListContainer, booksKey = 'books') {
-      super(booksKey);
-      this.form = form;
-      this.inputAuthor = inputAuthor;
-      this.inputTitle = inputTitle;
-      this.bookListContainer = bookListContainer;
-      this.booksKey = booksKey;
-    }
+  constructor(
+    form,
+    inputAuthor,
+    inputTitle,
+    bookListContainer,
+    booksKey = 'books',
+  ) {
+    super(booksKey);
+    this.form = form;
+    this.inputAuthor = inputAuthor;
+    this.inputTitle = inputTitle;
+    this.bookListContainer = bookListContainer;
+    this.booksKey = booksKey;
+  }
 
-    removeBook = (id, btnHTML) => {
-      btnHTML.target.parentNode.remove();
-      this.removeFromLS(id);
-    };
+  removeBook = (id, btnHTML) => {
+    btnHTML.target.parentNode.remove();
+    this.removeFromLS(id);
+  };
 
-    renderHTML = (obj) => `
-            <div>
+  renderHTML = (obj) => `
+            <div class = "bookcontainer">
                 <div class='book-title'>${obj.title}</div>
+                <p>by</p>
                 <div class='book-author'>${obj.author}</div>
                 <button class='remove-book' data-id="${obj.id}">Remove</button>
-                <hr>
             </div>
             `;
 
-    findRemoveButtons = () => {
-      const removeBookBtn = document.querySelectorAll('.remove-book');
+  findRemoveButtons = () => {
+    const removeBookBtn = document.querySelectorAll('.remove-book');
 
-      removeBookBtn.forEach((btn) => {
-        btn.addEventListener('click', (event) => {
-          const btnId = event.target.getAttribute('data-id');
+    removeBookBtn.forEach((btn) => {
+      btn.addEventListener('click', (event) => {
+        const btnId = event.target.getAttribute('data-id');
 
-          this.removeBook(btnId, event);
-        });
+        this.removeBook(btnId, event);
       });
+    });
+  };
+
+  addBookToHTML = (obj) => {
+    this.bookListContainer.insertAdjacentHTML(
+      'afterbegin',
+      this.renderHTML(obj),
+    );
+    this.findRemoveButtons();
+  };
+
+  addToBookList = (idBook, authorInput, titleInput) => {
+    const bookObj = {
+      id: idBook,
+      title: titleInput,
+      author: authorInput,
     };
 
-    addBookToHTML = (obj) => {
-      this.bookListContainer.insertAdjacentHTML('afterbegin', this.renderHTML(obj));
-      this.findRemoveButtons();
-    };
+    this.books[this.booksKey] = this.getFromLS(this.booksKey);
+    this.books[this.booksKey].push(bookObj);
+    this.addBookToHTML(bookObj);
+    this.addToLS(this.books[this.booksKey]);
+  };
 
-    addToBookList = (idBook, authorInput, titleInput) => {
-      const bookObj = {
-        id: idBook,
-        title: titleInput,
-        author: authorInput,
-      };
+  getUniqueId = () => {
+    const data = this.getFromLS(this.booksKey);
+    let max = 0;
 
-      this.books[this.booksKey] = this.getFromLS(this.booksKey);
-      this.books[this.booksKey].push(bookObj);
-      this.addBookToHTML(bookObj);
-      this.addToLS(this.books[this.booksKey]);
-    };
-
-    getUniqueId = () => {
-      const data = this.getFromLS(this.booksKey);
-      let max = 0;
-
-      data.forEach((_, i) => {
-        if (data[i].id > max) {
-          max = data[i].id;
-        }
-      });
-      max += 1;
-      return max;
-    };
-
-    init() {
-      const data = this.getFromLS(this.booksKey);
-      if (data.length > 0) {
-        data.forEach((book) => {
-          this.addBookToHTML(book);
-        });
+    data.forEach((_, i) => {
+      if (data[i].id > max) {
+        max = data[i].id;
       }
+    });
+    max += 1;
+    return max;
+  };
 
-      this.form.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const bookId = this.getUniqueId();
-
-        const authorText = this.inputAuthor.value;
-        const titleText = this.inputTitle.value;
-
-        this.addToBookList(bookId, authorText, titleText);
+  init() {
+    const data = this.getFromLS(this.booksKey);
+    if (data.length > 0) {
+      data.forEach((book) => {
+        this.addBookToHTML(book);
       });
     }
+
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const bookId = this.getUniqueId();
+
+      const authorText = this.inputAuthor.value;
+      const titleText = this.inputTitle.value;
+
+      this.addToBookList(bookId, authorText, titleText);
+      this.form.reset();
+    });
+  }
 }
 
 const form = document.getElementById('add-book');
